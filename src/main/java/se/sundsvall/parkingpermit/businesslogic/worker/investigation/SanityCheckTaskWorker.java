8d5f7d64db13e5c83665fa8a5f0b65ead726a818
Michael Wallner <Michael.Wallner@Sundsvall.se>
2023-09-04 13:47:13 +0200

    UF-5677: WIP

--- src/main/java/se/sundsvall/parkingpermit/businesslogic/worker/investigation/SanityCheckTaskWorker.java
+++ src/main/java/se/sundsvall/parkingpermit/businesslogic/worker/investigation/SanityCheckTaskWorker.java
@@ -1,14 +1,22 @@
 package se.sundsvall.parkingpermit.businesslogic.worker.investigation;
 
 import generated.se.sundsvall.camunda.VariableValueDto;
+import generated.se.sundsvall.casedata.ErrandDTO;
+import generated.se.sundsvall.casedata.StakeholderDTO;
 import org.camunda.bpm.client.spring.annotation.ExternalTaskSubscription;
 import org.camunda.bpm.client.task.ExternalTask;
 import org.camunda.bpm.client.task.ExternalTaskService;
 import org.camunda.bpm.engine.variable.type.ValueType;
 import org.springframework.stereotype.Component;
+import se.sundsvall.parkingpermit.Constants;
 import se.sundsvall.parkingpermit.businesslogic.worker.AbstractTaskWorker;
 
 import java.util.Map;
+
+import static generated.se.sundsvall.casedata.StakeholderDTO.RolesEnum.ADMINISTRATOR;
+import static generated.se.sundsvall.casedata.StakeholderDTO.TypeEnum.PERSON;
+import static se.sundsvall.parkingpermit.Constants.CAMUNDA_VARIABLE_SANITY_CHECK_PASSED;
+import static se.sundsvall.parkingpermit.Constants.CASEDATA_STATUS_COMPLETION_RECEIVED;
 
 @Component
 @ExternalTaskSubscription("InvestigationSanityCheckTask")
@@ -17,16 +25,29 @@ public class SanityCheckTaskWorker extends AbstractTaskWorker {
 	protected void executeBusinessLogic(ExternalTask externalTask, ExternalTaskService externalTaskService) {
 		try {
 			logInfo("Execute Worker for SanityCheckTaskWorker");
-			//TODO
-			// 1. Set updateAvailable to false -> clearUpdateAvailable(externalTask);
-			// 2. fetch errand -> getErrand(externalTask)
-			// 3. Run sanity checks
-			// 4. If passed set "sanityCheckPassed" to true, else set to false and update errand with missing parameters
 			clearUpdateAvailable(externalTask);
-			externalTaskService.complete(externalTask, Map.of("sanityCheckPassed", true));
+
+			final var errand = getErrand(externalTask);
+			logInfo("Executing sanity check for errand with id {}", errand.getId());
+
+			final var sanityCheckPassed = executeSanityChecks(errand);
+
+			externalTaskService.complete(externalTask, Map.of(CAMUNDA_VARIABLE_SANITY_CHECK_PASSED, sanityCheckPassed));
 		} catch (Exception exception) {
 			logException(externalTask, exception);
 			failureHandler.handleException(externalTaskService, externalTask, exception.getMessage());
 		}
+	}
+
+	private boolean executeSanityChecks(ErrandDTO errand) {
+		return errand.getStatuses().stream().allMatch(status -> status.getStatusType().equals(Constants.CASEDATA_STATUS_CASE_RECEIVED)
+			|| status.getStatusType().equals(CASEDATA_STATUS_COMPLETION_RECEIVED))
+			&& checkIfErrandHasManager(errand);
+	}
+
+	private boolean checkIfErrandHasManager(ErrandDTO errand) {
+		return errand.getStakeholders().stream()
+			.filter(stakeholder -> PERSON.equals(stakeholder.getType()))
+			.anyMatch(stakeholder -> stakeholder.getRoles().stream().anyMatch(ADMINISTRATOR::equals)); //TODO Check if this is correct
 	}
 }
