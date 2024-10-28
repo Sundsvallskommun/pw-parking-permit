0a3a6532158b69d04be3b747ecb2b0e4a047f5e4
Robin Rönnberg <robin.ronnberg@sundsvall.se>
2024-10-28 10:34:07 +0100

    UF-10547: Add reusable mocks in IT for investigation phase

---
+++ src/integration-test/java/apptest/mock/Investigation.java
@@ -0,0 +1,168 @@
+package apptest.mock;
+
+import java.util.Map;
+
+import static apptest.mock.api.BusinessRules.mockBusinessRulesPost;
+import static apptest.mock.api.CaseData.mockCaseDataDecisionPatch;
+import static apptest.mock.api.CaseData.mockCaseDataGet;
+import static apptest.mock.api.CaseData.mockCaseDataPatch;
+import static apptest.mock.api.CaseData.mockCaseDataPutStatus;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
+
+public class Investigation {
+
+    public static String mockInvestigation(String caseId, String scenarioName) {
+        var scenarioAfterUpdatePhase = mockInvestigationUpdatePhase(caseId, scenarioName, "actualization_check-phase-action_task-worker---api-casedata-patch-errand");
+        var scenarioAfterUpdateStatus = mockInvestigationUpdateStatus(caseId, scenarioName, scenarioAfterUpdatePhase);
+        var scenarioAfterSanityChecks = mockInvestigationSanityChecks(caseId, scenarioName, scenarioAfterUpdateStatus);
+        var scenarioAfterExecuteRules = mockInvestigationExecuteRules(caseId, scenarioName, scenarioAfterSanityChecks);
+        var scenarioAfterConstructDecision = mockInvestigationConstructDecision(caseId, scenarioName, scenarioAfterExecuteRules);
+        return mockInvestigationCheckPhaseAction(caseId, scenarioName, scenarioAfterConstructDecision);
+    }
+
+    public static String mockInvestigationUpdatePhase(String caseId, String scenarioName, String requiredScenarioState) {
+        var state = mockCaseDataGet(caseId, scenarioName, requiredScenarioState,
+                "investigation_update-phase-task-worker---api-casedata-get-errand",
+                Map.of(
+                        "phaseParameter", "Aktualisering",
+                        "phaseStatusParameter", "COMPLETE",
+                        "phaseActionParameter", "COMPLETED",
+                        "displayPhaseParameter", "Aktualisering"));
+
+        return mockCaseDataPatch(caseId, scenarioName, state,
+                "investigation_update-phase-task-worker---api-casedata-patch-errand",
+                equalToJson("""
+                            {
+                                "externalCaseId": "2971",
+                                "phase": "Utredning",
+                                "extraParameters": {
+                                    "process.phaseStatus": "ONGOING",
+                                    "process.phaseAction": "UNKNOWN",
+                                    "process.displayPhase": "Utredning"
+                                }
+                            }
+                            """));
+    }
+
+    public static String mockInvestigationUpdateStatus(String caseId, String scenarioName, String requiredScenarioState) {
+        var state = mockCaseDataGet(caseId, scenarioName, requiredScenarioState,
+                "investigation_update-status-task-worker---api-casedata-get-errand",
+                Map.of("statusTypeParameter", "Ärende inkommit",
+                        "phaseParameter", "Utredning",
+                        "phaseStatusParameter", "ONGOING",
+                        "phaseActionParameter", "UNKNOWN",
+                        "displayPhaseParameter", "Utredning"));
+
+        return mockCaseDataPutStatus(caseId, scenarioName, state,
+                "investigation_update-status-task-worker---api-casedata-put-status",
+                equalToJson("""
+                        [
+                          {
+                            "statusType": "Under utredning",
+                            "description": "Ärendet utreds",
+                            "dateTime": "${json-unit.any-string}"
+                          }
+                        ]
+                        """));
+    }
+
+    public static String mockInvestigationSanityChecks(String caseId, String scenarioName, String requiredScenarioState) {
+        return mockCaseDataGet(caseId, scenarioName, requiredScenarioState,
+                "investigation_sanity-checks-task-worker---api-casedata-get-errand",
+                Map.of("statusTypeParameter", "Ärende inkommit",
+                        "phaseParameter", "Utredning",
+                        "phaseStatusParameter", "ONGOING",
+                        "phaseActionParameter", "UNKNOWN",
+                        "displayPhaseParameter", "Utredning"));
+    }
+
+    public static String mockInvestigationExecuteRules(String caseId, String scenarioName, String requiredScenarioState) {
+        var state = mockCaseDataGet(caseId, scenarioName, requiredScenarioState,
+                "investigation_execute-rules-task-worker---api-casedata-get-errand",
+                Map.of("statusTypeParameter", "Ärende inkommit",
+                        "phaseParameter", "Utredning",
+                        "phaseStatusParameter", "ONGOING",
+                        "phaseActionParameter", "UNKNOWN",
+                        "displayPhaseParameter", "Utredning"));
+
+        return mockBusinessRulesPost(scenarioName, state,
+                "investigation_execute-rules-task-worker---api-businessrules-engine",
+                equalToJson("""
+                        {
+                            "context": "PARKING_PERMIT",
+                            "facts": [
+                                {
+                                    "key": "type",
+                                    "value": "PARKING_PERMIT"
+                                },
+                                {
+                                    "key": "stakeholders.applicant.personid",
+                                    "value": "6b8928bb-9800-4d52-a9fa-20d88c81f1d6"
+                                },
+                                {
+                                    "key": "application.applicant.capacity",
+                                    "value": "DRIVER"
+                                },
+                                {
+                                    "key": "disability.duration",
+                                    "value": "P6M"
+                                },
+                                {
+                                    "key": "disability.walkingAbility",
+                                    "value": "false"
+                                }
+                            ]
+                        }
+                        """),
+        Map.of());
+    }
+
+    public static String mockInvestigationConstructDecision(String caseId, String scenarioName, String requiredScenarioState) {
+        var state = mockCaseDataGet(caseId, scenarioName, requiredScenarioState,
+                "construct-recommended-decision-task-worker---api-casedata-get-errand",
+                Map.of(
+                        "phaseParameter", "Utredning",
+                        "phaseStatusParameter", "ONGOING",
+                        "phaseActionParameter", "UNKNOWN",
+                        "displayPhaseParameter", "Utredning"));
+
+        return mockCaseDataDecisionPatch(caseId, scenarioName, state,
+                "investigation_construct-recommended-decision_task-worker---api-casedata-patch-decision",
+                equalToJson("""
+                        {
+                            "version": 2,
+                            "created": "${json-unit.any-string}",
+                            "decisionType": "RECOMMENDED",
+                            "decisionOutcome": "APPROVAL",
+                            "description": "Rekommenderat beslut är bevilja. Den sökande är helt rullstolsburen, funktionsnedsättningens varaktighet är 6 månader eller längre och den sökande har inga aktiva parkeringstillstånd.",
+                            "law": [],
+                            "attachments": [],
+                            "extraParameters": {}
+                        }
+                        """));
+    }
+
+    public static String mockInvestigationCheckPhaseAction(String caseId, String scenarioName, String requiredScenarioState) {
+        var state = mockCaseDataGet(caseId, scenarioName, requiredScenarioState,
+                "investigation_check-phase-action_task-worker---api-casedata-get-errand",
+                Map.of(
+                        "phaseParameter", "Utredning",
+                        "phaseStatusParameter", "ONGOING",
+                        "phaseActionParameter", "COMPLETE",
+                        "displayPhaseParameter", "Utredning"));
+
+        return mockCaseDataPatch(caseId, scenarioName, state,
+                "investigation_check-phase-action_task-worker---api-casedata-patch-errand",
+                equalToJson("""
+                            {
+                                "externalCaseId": "2971",
+                                "phase": "Utredning",
+                                "extraParameters": {
+                                    "process.phaseStatus": "COMPLETED",
+                                    "process.phaseAction": "COMPLETE",
+                                    "process.displayPhase": "Utredning"
+                                }
+                            }
+                            """));
+    }
+}
