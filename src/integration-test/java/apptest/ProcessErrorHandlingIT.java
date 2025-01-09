336033e5e13c31765328e68df0fc7ef5580adce7
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2025-01-09 13:54:02 +0100

    UF-11489: IT-tests for process instance id existVerification.

---
+++ src/integration-test/java/apptest/ProcessErrorHandlingIT.java
@@ -0,0 +1,61 @@
+package apptest;
+
+import static java.time.Duration.ZERO;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+import static org.awaitility.Awaitility.setDefaultPollDelay;
+import static org.awaitility.Awaitility.setDefaultPollInterval;
+import static org.awaitility.Awaitility.setDefaultTimeout;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.time.Duration;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.annotation.DirtiesContext;
+import org.zalando.problem.Problem;
+import se.sundsvall.dept44.test.annotation.wiremock.WireMockAppTestSuite;
+import se.sundsvall.parkingpermit.Application;
+
+@DirtiesContext
+@WireMockAppTestSuite(files = "classpath:/Wiremock/", classes = Application.class)
+class ProcessErrorHandlingIT extends AbstractCamundaAppTest {
+
+	private static final int DEFAULT_TESTCASE_TIMEOUT_IN_SECONDS = 30;
+	private static final String TENANT_ID_PARKING_PERMIT = "PARKING_PERMIT";
+
+	@BeforeEach
+	void setup() {
+		setDefaultPollInterval(500, MILLISECONDS);
+		setDefaultPollDelay(ZERO);
+		setDefaultTimeout(Duration.ofSeconds(DEFAULT_TESTCASE_TIMEOUT_IN_SECONDS));
+
+		await()
+			.ignoreExceptions()
+			.atMost(DEFAULT_TESTCASE_TIMEOUT_IN_SECONDS, SECONDS)
+			.until(() -> camundaClient.getDeployments(null, null, TENANT_ID_PARKING_PERMIT).size(), equalTo(1));
+	}
+
+	@Test
+	void test_updateProcessNotFound() throws JsonProcessingException, ClassNotFoundException {
+
+		// Arrange
+		final var nonExistingProcessInstanceId = UUID.randomUUID().toString();
+
+		// Update process
+		final var response = setupCall()
+			.withServicePath("/2281/process/update/" + nonExistingProcessInstanceId)
+			.withHttpMethod(POST)
+			.withExpectedResponseStatus(NOT_FOUND)
+			.sendRequest()
+			.andReturnBody(Problem.class);
+
+		assertThat(response).isNotNull();
+		assertThat(response.getDetail()).isEqualTo("Process instance with ID '%s' does not exist!".formatted(nonExistingProcessInstanceId));
+	}
+}
