edc7384801b9159d9981d6ed0ffa1edca0947c9f
Michael Wallner <Michael.Wallner@sundsvall.se>
2025-06-11 10:38:12 +0200

    UF-16484: Added rpa config that supports folderId per municipality

--- src/main/java/se/sundsvall/parkingpermit/service/RpaService.java
+++ src/main/java/se/sundsvall/parkingpermit/service/RpaService.java
@@ -3,12 +3,15 @@ package se.sundsvall.parkingpermit.service;
 import static java.util.Collections.emptyList;
 import static java.util.Optional.ofNullable;
 import static org.springframework.util.ObjectUtils.nullSafeEquals;
+import static org.zalando.problem.Status.INTERNAL_SERVER_ERROR;
 import static se.sundsvall.parkingpermit.service.mapper.RpaMapper.toQueuesAddQueueItemParameters;
 
 import java.util.List;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.zalando.problem.Problem;
 import org.zalando.problem.Status;
 import org.zalando.problem.ThrowableProblem;
 import se.sundsvall.parkingpermit.integration.rpa.RpaClient;
@@ -31,11 +34,14 @@ public class RpaService {
 		this.rpaProperties = rpaProperties;
 	}
 
-	public void addQueueItems(List<String> queueNames, Long caseId) {
+	public void addQueueItems(List<String> queueNames, Long caseId, String municipalityId) {
+		final var folderId = Optional.ofNullable(rpaProperties.folderIds().get(municipalityId))
+			.orElseThrow(() -> Problem.valueOf(INTERNAL_SERVER_ERROR, "No folder ID found for municipality ID: " + municipalityId));
+
 		ofNullable(queueNames).orElse(emptyList())
 			.forEach(queueName -> {
 				try {
-					rpaClient.addQueueItem(rpaProperties.folderId(), toQueuesAddQueueItemParameters(queueName, caseId));
+					rpaClient.addQueueItem(folderId, toQueuesAddQueueItemParameters(queueName, caseId));
 				} catch (final ThrowableProblem e) {
 					if (nullSafeEquals(Status.CONFLICT, e.getStatus()) && isDuplicateMessageCode(e.getDetail())) {
 						// Queue item already exists
